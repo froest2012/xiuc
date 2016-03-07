c627ebb438b76f86bd3ab08193fea6efbcd006c2
xiuc <972994718@qq.com>
2016-03-07 09:22:14 +0800

    update

--- tiny/main/net/xiuc/poj/Poj3624.java
+++ tiny/main/net/xiuc/poj/Poj3624.java
@@ -22,16 +22,27 @@ public class Poj3624 {
                 weight[i] = in.nextInt();
                 sum += cost[i];
             }
-            int v = m;
-            int[] f = new int[v + 1];
-            for (int i = 1; i <= n; i++) {
-                for (int j = v; j >= Math.max(v - sum, cost[i]); j--) {
-                    f[j] = Math.max(f[j], f[j - cost[i]] + weight[i]);
-                }
-                sum -= cost[i];
-            }
-            System.out.println(f[v]);
+            System.out.println(pack01(cost,weight,m,sum));
         }
+    }
+
+    /**
+     * 01背包,cost和weight都需要开大一位数组,都是从1开始计数的
+     * @param cost      花费
+     * @param weight    价值
+     * @param v         总容量
+     * @param sum       所有花费总和
+     * @return          最大价值
+     */
+    private static int pack01(int[] cost,int[] weight, int v, int sum){
+        int[] f = new int[v + 1];
+        for (int i = 1; i < cost.length; i++) {
+            for (int j = v; j >= Math.max(v - sum, cost[i]); j--) {
+                f[j] = Math.max(f[j], f[j - cost[i]] + weight[i]);
+            }
+            sum -= cost[i];
+        }
+        return f[v];
     }
 
 }
